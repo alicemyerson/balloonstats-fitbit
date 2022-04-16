d70152dc39f2a7ed3eb4413c26bffbac63f6acdc
Alice Myerson <alice.h.burrell@gmail.com>
2022-04-16 17:43:54 +0100

    Working face at last

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # balloonstats-fitbit
-A fitbit clockface featuring rising balloons
+A fitbit clockface featuring rising balloons.  As you progress toward your goal in each one of steps, distance, calories, elevation and total active minutes, the balloons rise upward like (ahem) your fitness levels?!
+
+You're never too old to play with balloons on a sunny day.
+
+#### Fitbit Versa
+![Versa](https://raw.githubusercontent.com/alicemyerson/balloonstats-fitbit/main/screenshots/balloonstats1.png)
--- app/index.js
+++ app/index.js
@@ -6,15 +6,20 @@ import * as Utils from '../common/utils';
 
 const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 const months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+const stats = ['steps', 'distance', 'calories', 'elevationGain', 'activeZoneMinutes'];
 
 // Fetch handles to UI elements
 const stepCount = document.getElementById("stepCount");
 const batGauge = document.getElementById("batGauge");
 const dateString = document.getElementById("dateString");
-const hrNumber = document.getElementById("hrNumber");
-const minNumber = document.getElementById("minNumber");
+const timeNumber = document.getElementById("timeNumber");
+
+const textHandles = [document.getElementById("stepsTxt"), 
+                    document.getElementById("distanceTxt"),
+                    document.getElementById("caloriesTxt"),
+                    document.getElementById("elevationTxt"), 
+                    document.getElementById("activeminTxt")];
 //const debug = document.getElementById("debug");
-const glitter = document.getElementById("glitter");
 
 
 // clock colors
@@ -39,22 +44,38 @@ function init() {
 // Render clock face according to color and time
 function render(time) {
   
-  let currentType = "steps";
-  let currentDataProg = (userActivity.today.adjusted[currentType] || 0);
-  let currentDataGoal = userActivity.goals[currentType];
-  
-  
-  // the x position varies from y=300 to y=60 as the step goal goes from 0 to 100%
-  let goalPercent = currentDataProg/currentDataGoal*100;
-  if (goalPercent > 100) {
-    goalPercent = 100;
+  for (let i = 0;i<stats.length;i++){
+    let currentType = stats[i];
+    if  (currentType == 'activeZoneMinutes') {
+      // requires a total for activeZoneMinutes or else badness
+      let currentDataProg = (userActivity.today.adjusted[currentType].total || 0);
+      let currentDataGoal = userActivity.goals[currentType].total;
+    } else {
+      let currentDataProg = (userActivity.today.adjusted[currentType] || 0);
+      let currentDataGoal = userActivity.goals[currentType];
+    }
+    
+    // Get the progress as a percentage, capped at 100
+    let goalPercent = currentDataProg/currentDataGoal*100;
+    if (goalPercent > 100) {
+      goalPercent = 100;
+    }
+
+    //textHandles[i].text = Math.round(goalPercent);
+    textHandles[i].text = currentDataProg;
+
+    // additionally set the upper text in the top left screen corner
+    if (currentType == 'steps') {
+      stepCount.text = currentDataProg + ' steps';
+    }
+    // the x position varies from y=290 to y=-90 as the step goal goes from 0 to 100%
+    const balloonHandle = document.getElementById(currentType)
+    balloonHandle.y = -3.8*goalPercent + 290;
   }
-  glitter.y = -2.4*goalPercent + 300;
-  
+
   dateString.text = days[time.getDay()] + ' '+ time.getDate().toString() + ' '+  months[time.getMonth()];
-  hrNumber.text = time.getHours().toString();
-  minNumber.text = Utils.zeroPad(time.getMinutes());
-  stepCount.text = currentDataProg + ' steps';
+  timeNumber.text = time.getHours().toString() +':' + Utils.zeroPad(time.getMinutes());
+
   batGauge.text = Math.floor(battery.chargeLevel) + "%";
   
   //debug.text = currentDataProg + ' of ' + currentDataGoal;
